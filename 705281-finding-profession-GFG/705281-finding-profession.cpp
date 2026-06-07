a12fc36c0b4edfeb6e01f3d374f6cfc90a18a022
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-06-07 22:55:58 +0530

    Added solution - LeetHub

---
+++ 705281-finding-profession-GFG/705281-finding-profession.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string profession(int level, int pos) {
+        bool isEngineer = true;
+
+        while (pos > 1) {
+            if (pos % 2 == 0) {
+                isEngineer = !isEngineer;
+            }
+            pos = (pos + 1) / 2;
+        }
+
+        return isEngineer ? "Engineer" : "Doctor";
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
