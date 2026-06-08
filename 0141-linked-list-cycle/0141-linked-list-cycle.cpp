a9c3ae043bd49b7aa2a08da020338087991cd049
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-06-08 23:53:20 +0530

    Time: 13 ms (19.33%), Space: 11.9 MB (21.77%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while( fast!=nullptr && fast->next!=nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+        if(slow==fast){
+                return true;
+            }
+        }
+      return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
