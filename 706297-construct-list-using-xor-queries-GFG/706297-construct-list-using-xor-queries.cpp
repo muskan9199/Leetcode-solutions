5edd8b66b71300ca543d4a7a3e572031ae3ff6a2
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-06-16 23:48:25 +0530

    Added solution - LeetHub

---
+++ 706297-construct-list-using-xor-queries-GFG/706297-construct-list-using-xor-queries.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> constructList(vector<vector<int>>& queries) {
+        vector<int> ans;
+        int cum_xor = 0;
+        
+        for (int i = queries.size() - 1; i >= 0; --i) {
+            if (queries[i][0] == 0) {
+                ans.push_back(queries[i][1] ^ cum_xor);
+            } else {
+                cum_xor ^= queries[i][1];
+            }
+        }
+        
+        ans.push_back(0 ^ cum_xor);
+        
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
