a87defed3d10784d6850489b49aed6941fc1913b
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-06-16 23:49:43 +0530

    Time: 41 ms (78.46%), Space: 54 MB (57.11%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let acc = init;
+    
+    for (let i = 0; i < nums.length; i++) {
+        acc = fn(acc, nums[i]);
+    }
+    
+    return acc;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
