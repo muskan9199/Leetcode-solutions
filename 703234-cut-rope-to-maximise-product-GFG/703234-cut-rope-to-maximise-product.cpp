34c3a17d3f9bd3fec19db7b75fd9434fd63b5cb9
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-06-17 23:49:29 +0530

    Added solution - LeetHub

---
+++ 703234-cut-rope-to-maximise-product-GFG/703234-cut-rope-to-maximise-product.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        
+        int res = 1;
+        while (n > 4) {
+            res *= 3;
+            n -= 3;
+        }
+        return res * n;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
