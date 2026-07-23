fdc8527826bc86b7b34bfb920ba24d6edd02ed5b
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-07-23 14:31:38 +0530

    Force committed solution - Time: O(1), Space: O(n)

---
+++ 700208-implement-stack-using-array-GFG/700208-implement-stack-using-array.cpp
@@ -0,0 +1,45 @@
+class myStack {
+    int *arr;
+    int topIndex;
+    int size;
+public:
+    myStack(int n) {
+        size = n;
+        arr = new int[size];
+        topIndex = -1;
+    }
+    bool isEmpty() {
+        return topIndex == -1;
+    }
+    bool isFull() {
+        return topIndex == size - 1;
+    }
+    void push(int x) {
+        if(isFull()) {
+            return;
+        }
+
+        topIndex++;
+        arr[topIndex] = x;
+    }
+
+    void pop() {
+        if(isEmpty()) {
+            return;
+        }
+
+        topIndex--;
+    }
+
+    int peek() {
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return arr[topIndex];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
