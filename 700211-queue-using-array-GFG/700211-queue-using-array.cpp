246be1efea3d3992cc72759fac58abd1591eb4dd
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-07-24 12:44:51 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 700211-queue-using-array-GFG/700211-queue-using-array.cpp
@@ -0,0 +1,77 @@
+class myQueue {
+
+    int front;
+    int rear;
+    int arr[1000];
+    int size;
+    int currentSize;
+
+public:
+
+    myQueue(int n) {
+        size = n;
+        front = rear = -1;
+        currentSize = 0;
+    }
+
+    bool isEmpty() {
+        return currentSize == 0;
+    }
+
+    bool isFull() {
+        return currentSize == size;
+    }
+
+    void enqueue(int x) {
+
+        if(isFull()) {
+            return;
+        }
+
+        if(front == -1) {
+            front = 0;
+        }
+
+        rear++;
+        arr[rear] = x;
+        currentSize++;
+    }
+
+    void dequeue() {
+
+        if(isEmpty()) {
+            return;
+        }
+
+        if(front == rear) {
+            front = rear = -1;
+        }
+        else {
+            front++;
+        }
+
+        currentSize--;
+    }
+
+    int getFront() {
+
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return arr[front];
+    }
+
+    int getRear() {
+
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return arr[rear];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
