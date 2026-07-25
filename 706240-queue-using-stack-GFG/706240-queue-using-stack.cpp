023a8d999086de17250bff111015fa1297e43d2f
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-07-25 12:54:13 +0530

    Added solution - Time: O(1), Space: O(n)

---
+++ 706240-queue-using-stack-GFG/706240-queue-using-stack.cpp
@@ -0,0 +1,53 @@
+class myQueue {
+
+    stack<int> s1, s2;
+
+public:
+
+    myQueue() {
+    }
+
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    void dequeue() {
+
+        if(s2.empty()) {
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        if(!s2.empty()) {
+            s2.pop();
+        }
+    }
+
+    int front() {
+
+        if(s2.empty()) {
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        if(s2.empty()) {
+            return -1;
+        }
+
+        return s2.top();
+    }
+
+    int size() {
+
+        return s1.size() + s2.size();
+
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
