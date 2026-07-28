50b1f2b8b519eddd3b45b5034e65c63c4a3eebbd
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-07-28 21:28:30 +0530

    Force committed solution - Time: O(n * m), Space: O(1)

---
+++ 2084999374-set-matrix-zeros-GFG/2084999374-set-matrix-zeros.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void setMatrixZeroes(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        bool firstColZero = false;
+
+        for (int i = 0; i < n; i++) {
+            if (mat[i][0] == 0) {
+                firstColZero = true;
+            }
+            for (int j = 1; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 1; j--) {
+                if (mat[i][0] == 0 || mat[0][j] == 0) {
+                    mat[i][j] = 0;
+                }
+            }
+            if (firstColZero) {
+                mat[i][0] = 0;
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
