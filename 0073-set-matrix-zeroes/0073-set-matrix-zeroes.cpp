2d135e98930ee59bcad038b4a6c933c1d8ef2876
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-07-29 13:47:32 +0530

    Time: 1 ms (33.54%), Space: 20.8 MB (51.74%) - LeetHub - Time: O(n*m), Space: O(n+m)

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -6,21 +6,22 @@ public:
         vector<int>row(n,0);
         vector<int>col(m,0);
 
-        for(  int i=0; i<n; i++){
-            for( int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] ==0){
+                    row[i] =1;
+                    col[j]=1;
                 }
             }
         }
-         for(int i=0; i<n; i++){
-            for( int j=0; j<m; j++){
-                if(row[i]== 1 || col[j] == 1){
-                    matrix[i][j] = 0;
+      for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
                 }
             }
-         }
+      }
+        
     }
 };
 
