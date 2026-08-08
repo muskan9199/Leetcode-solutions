f542545642c2f291f4bd7871470fe54aef43f489
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-08 22:58:21 +0530

    Force committed solution - Time: O((n + E) * α(n)), Space: O(n)

---
+++ 711972-min-edge-movements-to-connect-a-graph-GFG/711972-min-edge-movements-to-connect-a-graph.cpp
@@ -0,0 +1,61 @@
+class DisjointSet {
+    vector<int> parent, size;
+public:
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++) parent[i] = i;
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int minEdgesReq(int n, vector<vector<int>>& edges) {
+        DisjointSet ds(n);
+        int extraEdges = 0;
+
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                extraEdges++;
+            } else {
+                ds.unionBySize(u, v);
+            }
+        }
+
+        int components = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.findUPar(i) == i) {
+                components++;
+            }
+        }
+
+        int ans = components - 1;
+        if (extraEdges >= ans) return ans;
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
