7d9a8d366e72f9149693ed3e041b5ebfe1dc1ab7
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-12 13:39:05 +0530

    Time: 6 ms (71.57%), Space: 19.2 MB (21.75%) - LeetHub - Time: O(n), Space: O(1)

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,31 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int lengthOfLongestSubstring(std::string s) {
-        std::vector<int> charIndex(128, -1);
-        int maxLength = 0;
-        int left = 0;
-        
-        for (int right = 0; right < s.length(); ++right) {
-            char currentChar = s[right];
-            
-            if (charIndex[currentChar] >= left) {
-                left = charIndex[currentChar] + 1;
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int r = 0;
+        int l = 0;
+        int len = 0;
+        int maxlen = 0;
+
+        vector<int> hash(256, -1);
+
+        while(r < n) {
+
+            if(hash[s[r]] != -1) {
+                if(hash[s[r]] >= l) {
+                    l = hash[s[r]] + 1;
+                }
             }
-            
-            charIndex[currentChar] = right;
-            maxLength = std::max(maxLength, right - left + 1);
+
+            len = r - l + 1;
+            maxlen = max(len, maxlen);
+
+            hash[s[r]] = r;
+
+            r++;
         }
-        
-        return maxLength;
+
+        return maxlen;
     }
 };
 
