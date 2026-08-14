e54f3fd35afdd0e63ea30fa66be5e3cdab67b93e
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-14 21:58:23 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 705441-subset-sum-on-generated-sequence-GFG/705441-subset-sum-on-generated-sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& arr, int s, int x) {
+        vector<long long> S;
+        S.push_back(s);
+        long long current_sum = s;
+
+        for (int val : arr) {
+            long long next_val = current_sum + val;
+            S.push_back(next_val);
+            current_sum += next_val;
+            if (next_val > x) {
+                break;
+            }
+        }
+
+        for (int i = S.size() - 1; i >= 0; i--) {
+            if (x >= S[i]) {
+                x -= S[i];
+            }
+        }
+
+        return x == 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
