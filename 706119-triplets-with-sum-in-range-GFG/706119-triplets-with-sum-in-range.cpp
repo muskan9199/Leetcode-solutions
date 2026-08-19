62e5bfd52ab60966917ee53175bc3e9024680098
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-19 22:16:49 +0530

    Added solution

---
+++ 706119-triplets-with-sum-in-range-GFG/706119-triplets-with-sum-in-range.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+private:
+    int countTripletsLessThanOrEqual(std::vector<int>& arr, int target) {
+        int n = arr.size();
+        int count = 0;
+
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                if (sum <= target) {
+                    count += (right - left);
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return count;
+    }
+
+public:
+    int countTriplets(std::vector<int>& arr, int l, int r) {
+        std::sort(arr.begin(), arr.end());
+        return countTripletsLessThanOrEqual(arr, r) - countTripletsLessThanOrEqual(arr, l - 1);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
