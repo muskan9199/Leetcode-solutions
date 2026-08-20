bf44c4df31013608e0542e1d6a76961ce45b4af2
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-20 21:37:59 +0530

    Force committed solution

---
+++ 700227-node-and-ancestor-max-diff-GFG/700227-node-and-ancestor-max-diff.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  private:
+    int solve(Node* root, int &ans) {
+        if (!root) return INT_MAX;
+
+        if (!root->left && !root->right) return root->data;
+
+        int leftMin = solve(root->left, ans);
+        int rightMin = solve(root->right, ans);
+
+        int minChild = std::min(leftMin, rightMin);
+
+        ans = std::max(ans, root->data - minChild);
+
+        return std::min(minChild, root->data);
+    }
+
+  public:
+    int maxDiff(Node* root) {
+        int ans = INT_MIN;
+        solve(root, ans);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
