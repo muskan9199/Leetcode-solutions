277cd911233f896c4f9880e910134d1c424f04d9
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-24 23:43:36 +0530

    Added solution

---
+++ 704553-count-prefix-balanced-binary-strings-GFG/704553-count-prefix-balanced-binary-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int prefixStrings(int n) {
+        long long MOD = 1e9 + 7;
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = (dp[i] + (dp[j] * dp[i - 1 - j]) % MOD) % MOD;
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
