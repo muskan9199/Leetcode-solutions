fa49a0073366d629b486ef26a86b54830829dfae
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-26 22:47:52 +0530

    Time: 0 ms (100%), Space: 23 MB (53.86%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<bool>> rows(9, vector<bool>(9, false));
+        vector<vector<bool>> cols(9, vector<bool>(9, false));
+        vector<vector<bool>> boxes(9, vector<bool>(9, false));
+
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] == '.') continue;
+
+                int num = board[r][c] - '1';
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r][num] || cols[c][num] || boxes[boxIndex][num]) {
+                    return false;
+                }
+
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[boxIndex][num] = true;
+            }
+        }
+
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
