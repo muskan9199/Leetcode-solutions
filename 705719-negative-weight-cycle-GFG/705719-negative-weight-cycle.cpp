a3eb1f677499fc53056f9995dfdd7d8a4d99a0bb
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-26 22:44:58 +0530

    Force committed solution

---
+++ 705719-negative-weight-cycle-GFG/705719-negative-weight-cycle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isNegativeWeightCycle(int V, vector<vector<int>>& edges) {
+        vector<int> dist(V, 0);
+
+        for (int i = 0; i < V - 1; ++i) {
+            for (const auto& edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                if (dist[u] != 1e9 && dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+
+        for (const auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            if (dist[u] != 1e9 && dist[u] + w < dist[v]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
