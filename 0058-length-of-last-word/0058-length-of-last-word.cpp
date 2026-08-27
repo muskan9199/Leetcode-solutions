489dc3663e0cd78e31e48d7c1408b4651e97e535
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-27 23:45:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (92.63%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        int length = 0;
+
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
