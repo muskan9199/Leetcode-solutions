f9864db6fd80986bf301f78f28c4f2fa07e0ae4e
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-28 21:26:51 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 705346-minimum-cost-selection-GFG/705346-minimum-cost-selection.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& mat) {
+        int n = mat.size();
+        if (n == 0) return 0;
+
+        int prev0 = mat[0][0];
+        int prev1 = mat[0][1];
+        int prev2 = mat[0][2];
+
+        for (int i = 1; i < n; ++i) {
+            int curr0 = mat[i][0] + min(prev1, prev2);
+            int curr1 = mat[i][1] + min(prev0, prev2);
+            int curr2 = mat[i][2] + min(prev0, prev1);
+
+            prev0 = curr0;
+            prev1 = curr1;
+            prev2 = curr2;
+        }
+
+        return min({prev0, prev1, prev2});
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
