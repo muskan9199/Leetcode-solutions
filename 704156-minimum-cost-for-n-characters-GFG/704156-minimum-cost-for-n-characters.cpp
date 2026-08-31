be98469045d504145d3387450d2b48f77f25cc82
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-08-31 22:47:37 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 704156-minimum-cost-for-n-characters-GFG/704156-minimum-cost-for-n-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minCost(int n, int i, int d, int c) {
+        vector<long long> dp(n + 1, 0);
+        dp[1] = i;
+
+        for (int k = 2; k <= n; ++k) {
+            if (k % 2 == 0) {
+                dp[k] = min((long long)dp[k - 1] + i, (long long)dp[k / 2] + c);
+            } else {
+                dp[k] = min({(long long)dp[k - 1] + i, (long long)dp[(k + 1) / 2] + c + d});
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
