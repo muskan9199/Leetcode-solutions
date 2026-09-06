4c7929d845a8c0b3a10adc0d903b7308e401e71f
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-09-06 23:19:08 +0530

    Added solution - Time: O(32n), Space: O(1)

---
+++ 703796-sum-of-pairwise-ands-GFG/703796-sum-of-pairwise-ands.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    long long pairAndSum(vector<int> &arr) {
+        long long totalSum = 0;
+        int n = arr.size();
+
+        for (int bit = 0; bit < 32; bit++) {
+            long long count = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] & (1 << bit)) {
+                    count++;
+                }
+            }
+            totalSum += count * (count - 1) / 2 * (1LL << bit);
+        }
+
+        return totalSum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
