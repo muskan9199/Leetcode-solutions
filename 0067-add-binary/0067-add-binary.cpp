1353fbf268cef419f72c25036ab13d13850a406e
Muskan kumari <iammuskanchaudhary08@gmail.com>
2026-09-24 23:14:54 +0530

    Time: 0 ms (100%), Space: 10.1 MB (15.89%) - LeetHub - Time: O(max(N, M)), Space: O(max(N, M))

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+            carry = sum / 2;
+            result += to_string(sum % 2);
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
